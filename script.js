2ab04ca1133d54d19739c310fe5f7b1b8472dc3e
Anis Kehila <aniskehila22@gmail.com>
2022-11-26 13:55:56 +0100

    Completed the art

--- script.js
+++ script.js
@@ -2,12 +2,59 @@
 let container = document.getElementById('container');
 let controlPanel = document.getElementById('control-panel');
 let grid = document.createElement('div');
+let mouseDown = false;
+let clearBtn = document.querySelector('input[value = Clear]');
+let displaySize = document.querySelector('span');
+let sizeSelector = document.querySelector('input[type = range]');
+let modeBtns = document.querySelectorAll('.mode');
+let mode = 'color';
+// remove the active class
+function removeActive() {
+    modeBtns.forEach(modeBtn => modeBtn.classList.remove('active'));
+}
+// setting coloring mode
+modeBtns.forEach(modeBtn => modeBtn.addEventListener('click' , () => {
+    removeActive();
+    modeBtn.classList.add('active');
+    mode = modeBtn.getAttribute('data-mode');
+}));
+
+// append grid to DOM
 grid.classList.add('grid');
 container.appendChild(grid);
 
+// Clear the game space function
+function clear() {
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {pixel.style = 'background-color: white;'});
+}
+// Reset game  function
+function resetGame() {
+    grid.innerHTML = '';
+    startGame();
+}
+// Coloring pixels in the grid
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+function colorPixels(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    if(mode === 'color') {
+        let colorSelector = document.querySelector('input[type = color]');
+        let color = colorSelector.value;
+        this.style = `background-color: ${color};`;
+    } else if(mode === 'eraser') {
+        this.style = `background-color: white`
+    } else if(mode === 'rainbow'){
+        let random = Math.floor(Math.random() * 256);
+        let random1 = Math.floor(Math.random() * 256);
+        let random2 = Math.floor(Math.random() * 256);
+        this.style = `background-color: rgb(${random} ,${random1} ,${random2} )`;
+    }
+}
+// Game Logic
 function startGame() {
-    let sizeSelector = document.querySelector('input[type = range]')
     let size = sizeSelector.value;
+    displaySize.innerText = `${size} x ${size}`;
     for(let i=0; i<size*size; i++) {
         var pixel = document.createElement('div');
         pixel.classList.add('pixel');
@@ -21,17 +68,10 @@ function startGame() {
         grid-template-rows: repeat(${size}, 1fr);                             
     `;
 }
-// Game Logic
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
-function colorPixels(e) {
-    if(e.type === 'mouseover' && !mouseDown) return;
-    let colorSelector = document.querySelector('input[type = color]');
-    let color = colorSelector.value;
-    this.style = `background-color: ${color};`;
-}
 
+
+clearBtn.addEventListener('click' , clear);
+sizeSelector.addEventListener('input' , resetGame);
 
 
 window.onload = () => {startGame();}
